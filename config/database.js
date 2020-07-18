e34e8c970a4294a1c4b67596160f309db598dd28
Victor Fuly <victor_hugo_fuly@hotmail.com>
2020-07-18 14:19:59 -0300

    first login

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/javascriptNote', {
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
